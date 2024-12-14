65930c22a98c271f528ef4facaa8e6165f5da405
Abel Misiocha <wells.mail.13@gmail.com>
2024-12-14 21:34:41 +0300

    feat(core): Add responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,23 +10,23 @@ import styles from './index.module.css'
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext()
     return (
-        <header className={clsx('flex flex-row bg-slate-100 justify-center gap-24', styles.heroBanner)}>
+        <header className={clsx('flex flex-col items-center lg:items-start lg:flex-row bg-slate-100 justify-center gap-8 lg:gap-24', styles.heroBanner)}>
             <div className='max-w-[600px] mt-12'>
-                <Heading as='h1' className='hero__title text-6xl text-start'>
+                <Heading as='h1' className='hero__title text-6xl lg:text-start'>
                     {siteConfig.title}
                 </Heading>
-                <p className='font-medium text-3xl text-start'>Your Partner in All Matters Software</p>
-                <p className='text-start text-lg'>
+                <p className='font-medium text-3xl lg:text-start'>Your Partner in All Matters Software</p>
+                <p className='lg:text-start text-lg'>
                     Bring your ideas to market. From your product's concept to launch, we've got you covered. We build software that scales and grows with your business.
                 </p>
-                <div className={clsx('flex items-start')}>
+                <div className={clsx('flex justify-center items-start lg:justify-start')}>
                     <Link to='https://zenetralabs.com' target='_blank' className='bg-zenetra text-white hover:opacity-80 hover:text-white font-medium px-8 py-3 text-lg rounded-md'>
                         Visit the Labs
                     </Link>
                 </div>
             </div>
 
-            <img src={'/img/steps.png'} alt={'zenetra labs image'} className='max-w-[500px]' />
+            <img src={'/img/steps.png'} alt={'zenetra labs image'} className='max-w-[400px] md:max-w-[600px] lg:max-w-[500px]' />
         </header>
     )
 }
@@ -34,7 +34,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext()
     return (
-        <Layout title={`${siteConfig.title}`} description='Description will go into a meta tag in <head />'>
+        <Layout title={`${siteConfig.title}`} description='Your partner in all matters software'>
             <HomepageHeader />
             <main>
                 <HomepageFeatures />
