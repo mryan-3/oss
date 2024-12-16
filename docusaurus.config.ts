aa3c67be751f30902e1fd9f4851bef96cedbd475
Abel Misiocha <wells.mail.13@gmail.com>
2024-12-16 16:47:31 +0300

    fix(core): build error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
     organizationName: 'zenetralabs', // Usually your GitHub org/user name.
     projectName: 'oss', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
