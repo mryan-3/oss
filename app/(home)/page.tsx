1c370e86b94f9da31cc58ed828e82935df6cb9da
Abel Misiocha <wells.mail.13@gmail.com>
2025-02-23 14:04:01 +0300

    feat(core): Add mpesa docs

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,10 +15,10 @@ function Hero() {
         <section className='mx-4 flex flex-col gap-20 pb-14 pt-16 md:px-10 md:py-24 lg:flex-row lg:gap-2.5 lg:px-16 xl:px-20 2xl:px-52 '>
             <div className='relative z-10 flex h-full flex-1 flex-col items-start justify-center md:mx-auto md:items-center lg:items-start lg:text-center'>
                 <header className='mx-auto  flex w-full flex-col justify-center md:items-center md:text-center lg:mx-0 lg:items-start lg:text-start'>
-                    <p className='mx-auto mb-8 w-fit rounded-full border border-black bg-[#88aaee] px-3 text-black py-1 text-sm font-medium shadow-md md:mx-0 md:mb-2 md:shadow-none'>
+                    <p className='mx-auto mb-8 w-fit rounded-full border border-black bg-[#88aaee] px-3 text-black py-1 text-sm font-medium shadow-md md:mx-0 md:mb-2 md:shadow-none dark:border-purple-50'>
                         Zenetra OSS
                     </p>
-                    <h1 className='scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-5xl mt-2 text-black lg:text-start lg:text-6xl'>
+                    <h1 className='scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:text-5xl mt-2 text-black lg:text-start lg:text-6xl dark:text-gray-200'>
                         We love & Support Open Source
                     </h1>
                     <p className='mt-2 max-w-lg text-center text-lg lg:max-w-xl lg:text-start'>
@@ -34,7 +34,7 @@ function Hero() {
                     <Link
                         href='/#contact-us'
                         className={
-                            'text-lg border-2 border-black py-2 px-6 bg-[#88aaee] hover:opacity-90 text-black active:opacity-100 hover:border-1'
+                            'text-lg border-2 border-black py-2 px-6 bg-[#88aaee] hover:opacity-90 text-black active:opacity-100 hover:border-1 dark:border-purple-50'
                         }
                     >
                         See our projects
@@ -42,7 +42,7 @@ function Hero() {
                 </div>
             </div>
 
-            <div className='max-w-xm h-full flex-1 border-2 border-black'>
+            <div className='max-w-xm h-full flex-1 border-2 border-black dark:border-purple-50'>
                 <div className='group relative col-span-6 row-span-2 flex h-fit flex-col overflow-hidden bg-[#88aaee] bg-no-repeat text-black transition-all md:h-[32.1rem] lg:h-[30rem] 2xl:h-[32rem]'>
                     <div className='z-10 flex h-full flex-1 flex-col px-4 pt-4 2xl:px-8 2xl:py-8'>
                         <h2 className='whitespace-pre-line text-center text-[1.8rem] font-medium md:text-start md:text-[2.375rem] md:leading-[1.3] 2xl:text-4xl'>
@@ -95,7 +95,7 @@ const OSSProjects = [
 function Projects() {
     return (
         <section className='mx-4 flex flex-col gap-20 md:px-10 pb-32 lg:gap-2.5 lg:px-16 xl:px-20 2xl:px-52 '>
-            <h2 className='text-2xl font-semibold tracking-tight md:text-4xl mt-2 text-black lg:text-start'>
+            <h2 className='text-2xl font-semibold tracking-tight md:text-4xl mt-2 text-black lg:text-start dark:text-gray-200'>
                 Our Projects
             </h2>
 
@@ -104,7 +104,7 @@ function Projects() {
                     <Link
                         href={project.link}
                         key={'projects_' + index}
-                        className='flex divide-y flex-col gap-4 border-2 hover:opacity-80 border-black bg-white'
+                        className='flex divide-y flex-col gap-4 border-2 hover:opacity-80 border-black bg-white dark:bg-gray-800 dark:border-purple-800/20 dark:divide-black'
                     >
                         <div className='p-4'>
                             <Image
@@ -117,7 +117,7 @@ function Projects() {
                         </div>
 
                         <div className='pb-4 px-4'>
-                            <h3 className='text-lg text-start font-semibold text-black'>
+                            <h3 className='text-lg text-start font-semibold text-black dark:text-gray-200'>
                                 {project.title}
                             </h3>
                             <p className='text-start line-clamp-4 text-sm mt-2'>
